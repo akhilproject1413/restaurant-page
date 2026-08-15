1efba25dbb5b38eb4d4817c56badac89f77e0652
akhil <akhilparelli55@gmail.com>
2026-08-15 15:56:35 +0530

    Add files via upload

---
+++ restuarant.js
@@ -0,0 +1,659 @@
+/*const indianFoods = [
+    {
+        name: "biryani",
+        image: "indian foods/biryani.png",
+        stars:4,
+        price:630
+
+
+    },
+    {
+        name: "chicken butter",
+        image: "indian foods/chicken butter.png"
+    },
+
+    {
+        name: "Chole Bhature",
+        image: "indian foods/Chole Bhature.png"
+    },
+    {
+        name: "gulab jam",
+        image: "indian foods/gulab jam.png"
+    },
+    {
+        name: "masala dosa",
+        image: "indian foods/masala dosa.png"
+    }
+];
+const swizFood = [
+  {
+    id: "swiz-1",
+    name: "Cheese Fondue",
+    image: "switzerland foods/Cheese Fondue.png"
+  },
+  {
+    id: "swiz-2",
+    name: "Raclette",
+    image: "switzerland foods/Raclette.png"
+  },
+  {
+    id: "swiz-3",
+    name: "Rösti",
+    image: "switzerland foods/Rösti.png"
+  },
+  {
+    id: "swiz-4",
+    name: "Swiss Chocolate",
+    image: "switzerland foods/Swiss Chocolate.png"
+  },
+  {
+    id: "swiz-5",
+    name: "Zurcher Geschnetzeltes",
+    image: "switzerland foods/Zurcher Geschnetzeltes.png"
+  }
+];
+const japanFoods = [
+  {
+    id: "japan-1",
+    name: "Miso Soup",
+    image: "japan foods/miso soup.png"
+  },
+  {
+    id: "japan-2",
+    name: "Okonomiyaki",
+    image: "japan foods/Okonomiyaki.png"
+  },
+  {
+    id: "japan-3",
+    name: "Ramen",
+    image: "japan foods/ramen.png"
+  },
+  {
+    id: "japan-4",
+    name: "Sushi",
+    image: "japan foods/sushi.png"
+  },
+  {
+    id: "japan-5",
+    name: "Tempura",
+    image: "japan foods/tempura.png"
+  }
+];
+const southKoreaFoods = [
+  {
+    id: "korea-1",
+    name: "Bibimbap",
+    image: "south korea foods/bibimbap.png"
+  },
+  {
+    id: "korea-2",
+    name: "Kimchi",
+    image: "south korea foods/kimchi.png"
+  },
+  {
+    id: "korea-3",
+    name: "Korean BBQ",
+    image: "south korea foods/Korean BBQ.png"
+  },
+  {
+    id: "korea-4",
+    name: "Samgyetang",
+    image: "south korea foods/Samgyetang.png"
+  },
+  {
+    id: "korea-5",
+    name: "Tteokbokki",
+    image: "south korea foods/Tteokbokki.png"
+  }
+];
+const americanFoods = [
+  {
+    id: "usa-1",
+    name: "Apple Pie",
+    image: "american foods/apple pie.png"
+  },
+  {
+    id: "usa-2",
+    name: "Fried Chicken",
+    image: "american foods/fried chicken.png"
+  },
+  {
+    id: "usa-3",
+    name: "Hamburger",
+    image: "american foods/hamburger.png"
+  },
+  {
+    id: "usa-4",
+    name: "Hot Dog",
+    image: "american foods/hot dog.png"
+  },
+  {
+    id: "usa-5",
+    name: "Mac and Cheese",
+    image: "american foods/mac and cheese.png"
+  }
+];
+const indianFoods = [
+  {
+    name: "Biryani",
+    image: "indian foods/biryani.png",
+    stars: 5,
+    price: 649
+  },
+  {
+    name: "Chicken Butter",
+    image: "indian foods/chicken butter.png",
+    stars: 4.5,
+    price: 879
+  },
+  {
+    name: "Chole Bhature",
+    image: "indian foods/Chole Bhature.png",
+    stars: 4.5,
+    price: 529
+  },
+  {
+    name: "Gulab Jamun",
+    image: "indian foods/gulab jam.png",
+    stars: 4,
+    price: 589
+  },
+  {
+    name: "Masala Dosa",
+    image: "indian foods/masala dosa.png",
+    stars: 5,
+    price: 619
+  }
+];
+
+const swizFood = [
+  {
+    id: "swiz-1",
+    name: "Cheese Fondue",
+    image: "switzerland foods/Cheese Fondue.png",
+    stars: 5,
+    price: 1648
+  },
+  {
+    id: "swiz-2",
+    name: "Raclette",
+    image: "switzerland foods/Raclette.png",
+    stars: 4.5,
+    price: 1586
+  },
+  {
+    id: "swiz-3",
+    name: "Rösti",
+    image: "switzerland foods/Rösti.png",
+    stars: 4.5,
+    price: 982
+  },
+  {
+    id: "swiz-4",
+    name: "Swiss Chocolate",
+    image: "switzerland foods/Swiss Chocolate.png",
+    stars: 5,
+    price: 1367
+  },
+  {
+    id: "swiz-5",
+    name: "Zürcher Geschnetzeltes",
+    image: "switzerland foods/Zürcher Geschnetzeltes.png",
+    stars: 4.5,
+    price: 1839
+  }
+];
+
+const japanFoods = [
+  {
+    id: "japan-1",
+    name: "Miso Soup",
+    image: "japan foods/miso soup.png",
+    stars: 4,
+    price: 576
+  },
+  {
+    id: "japan-2",
+    name: "Okonomiyaki",
+    image: "japan foods/Okonomiyaki.png",
+    stars: 4.5,
+    price: 928
+  },
+  {
+    id: "japan-3",
+    name: "Ramen",
+    image: "japan foods/ramen.png",
+    stars: 5,
+    price: 1184
+  },
+  {
+    id: "japan-4",
+    name: "Sushi",
+    image: "japan foods/sushi.png",
+    stars: 5,
+    price: 1793
+  },
+  {
+    id: "japan-5",
+    name: "Tempura",
+    image: "japan foods/tempura.png",
+    stars: 4.5,
+    price: 1276
+  }
+];
+
+const southKoreaFoods = [
+  {
+    id: "korea-1",
+    name: "Bibimbap",
+    image: "south korea foods/bibimbap.png",
+    stars: 4.5,
+    price: 894
+  },
+  {
+    id: "korea-2",
+    name: "Kimchi",
+    image: "south korea foods/kimchi.png",
+    stars: 4,
+    price: 548
+  },
+  {
+    id: "korea-3",
+    name: "Korean BBQ",
+    image: "south korea foods/Korean BBQ.png",
+    stars: 5,
+    price: 1968
+  },
+  {
+    id: "korea-4",
+    name: "Samgyetang",
+    image: "south korea foods/Samgyetang.png",
+    stars: 4.5,
+    price: 1127
+  },
+  {
+    id: "korea-5",
+    name: "Tteokbokki",
+    image: "south korea foods/Tteokbokki.png",
+    stars: 5,
+    price: 742
+  }
+];
+
+const americanFoods = [
+  {
+    id: "usa-1",
+    name: "Apple Pie",
+    image: "american foods/apple pie.png",
+    stars: 4.5,
+    price: 687
+  },
+  {
+    id: "usa-2",
+    name: "Fried Chicken",
+    image: "american foods/fried chicken.png",
+    stars: 5,
+    price: 1189
+  },
+  {
+    id: "usa-3",
+    name: "Hamburger",
+    image: "american foods/hamburger.png",
+    stars: 5,
+    price: 1048
+  },
+  {
+    id: "usa-4",
+    name: "Hot Dog",
+    image: "american foods/hot dog.png",
+    stars: 4.5,
+    price: 719
+  },
+  {
+    id: "usa-5",
+    name: "Mac and Cheese",
+    image: "american foods/mac and cheese.png",
+    stars: 4.5,
+    price: 834
+  }
+];
+
+/*
+indianFoods.forEach((indFoods) => {
+    indHtml +=`  <div class="image js-southKorean-foods">
+                   <img src="${indFoods.image}">
+
+            </div>
+            <div class="rating">
+                <span class="stars-rating">
+
+                </span>
+                <span class="price">
+
+                </span>
+                
+            </div>">`
+
+
+
+});
+swizFood.forEach((swiz)=>{
+    swizHtml+=`<img src="${swiz.image}">
+    <div class="image js-southKorean-foods">
+
+            </div>
+            <div class="rating">
+                <span class="stars-rating">
+
+                </span>
+                <span class="price">
+
+                </span>
+                
+            </div>`
+});
+americanFoods.forEach((swiz)=>{
+    americaHtml+=`<img src="${swiz.image}">
+    <div class="image js-southKorean-foods">
+
+            </div>
+            <div class="rating">
+                <span class="stars-rating">
+
+                </span>
+                <span class="price">
+
+                </span>
+                
+            </div>`
+});
+southKoreaFoods.forEach((swiz)=>{
+    southKoreanHtml+=`<img src="${swiz.image}">
+<div class="image js-southKorean-foods">
+
+            </div>
+            <div class="rating">
+                <span class="stars-rating">
+
+                </span>
+                <span class="price">
+
+                </span>
+                
+            </div>`
+});
+japanFoods.forEach((swiz)=>{
+    japanHtml+=`<img src="${swiz.image}"><div class="image js-southKorean-foods">
+
+            </div>
+            <div class="rating">
+                <span class="stars-rating">
+
+                </span>
+                <span class="price">
+
+                </span>
+                
+            </div>`
+});*/
+
+const indianFoods = [
+  {
+    name: "Biryani",
+    image: "indian foods/biryani.png",
+    stars: "★★★★★",
+    price: 649
+  },
+  {
+    name: "Chicken Butter",
+    image: "indian foods/chicken butter.png",
+    stars: "★★★★☆",
+    price: 879
+  },
+  {
+    name: "Chole Bhature",
+    image: "indian foods/Chole Bhature.png",
+    stars: "★★★★☆",
+    price: 529
+  },
+  {
+    name: "Gulab Jamun",
+    image: "indian foods/gulab jam.png",
+    stars: "★★★★☆",
+    price: 589
+  },
+  {
+    name: "Masala Dosa",
+    image: "indian foods/masala dosa.png",
+    stars: "★★★★★",
+    price: 619
+  }
+];
+
+const swizFood = [
+  {
+    id: "swiz-1",
+    name: "Cheese Fondue",
+    image: "switzerland foods/Cheese Fondue.png",
+    stars: "★★★★★",
+    price: 1648
+  },
+  {
+    id: "swiz-2",
+    name: "Raclette",
+    image: "switzerland foods/Raclette.png",
+    stars: "★★★★☆",
+    price: 1586
+  },
+  {
+    id: "swiz-3",
+    name: "Rösti",
+    image: "switzerland foods/Rösti.png",
+    stars: "★★★★☆",
+    price: 982
+  },
+  {
+    id: "swiz-4",
+    name: "Swiss Chocolate",
+    image: "switzerland foods/Swiss Chocolate.png",
+    stars: "★★★★★",
+    price: 1367
+  },
+  {
+    id: "swiz-5",
+    name: "Zurcher Geschnetzeltes",
+    image: "switzerland foods/Zurcher Geschnetzeltes.png",
+    stars: "★★★★☆",
+    price: 1839
+  }
+];
+
+const japanFoods = [
+  {
+    id: "japan-1",
+    name: "Miso Soup",
+    image: "japan foods/miso soup.png",
+    stars: "★★★★☆",
+    price: 576
+  },
+  {
+    id: "japan-2",
+    name: "Okonomiyaki",
+    image: "japan foods/Okonomiyaki.png",
+    stars: "★★★★☆",
+    price: 928
+  },
+  {
+    id: "japan-3",
+    name: "Ramen",
+    image: "japan foods/ramen.png",
+    stars: "★★★★★",
+    price: 1184
+  },
+  {
+    id: "japan-4",
+    name: "Sushi",
+    image: "japan foods/sushi.png",
+    stars: "★★★★★",
+    price: 1793
+  },
+  {
+    id: "japan-5",
+    name: "Tempura",
+    image: "japan foods/tempura.png",
+    stars: "★★★★☆",
+    price: 1276
+  }
+];
+
+const southKoreaFoods = [
+  {
+    id: "korea-1",
+    name: "Bibimbap",
+    image: "south korea foods/bibimbap.png",
+    stars: "★★★★☆",
+    price: 894
+  },
+  {
+    id: "korea-2",
+    name: "Kimchi",
+    image: "south korea foods/kimchi.png",
+    stars: "★★★★☆",
+    price: 548
+  },
+  {
+    id: "korea-3",
+    name: "Korean BBQ",
+    image: "south korea foods/Korean BBQ.png",
+    stars: "★★★★★",
+    price: 1968
+  },
+  {
+    id: "korea-4",
+    name: "Samgyetang",
+    image: "south korea foods/Samgyetang.png",
+    stars: "★★★★☆",
+    price: 1127
+  },
+  {
+    id: "korea-5",
+    name: "Tteokbokki",
+    image: "south korea foods/Tteokbokki.png",
+    stars: "★★★★★",
+    price: 742
+  }
+];
+
+const americanFoods = [
+  {
+    id: "usa-1",
+    name: "Apple Pie",
+    image: "american foods/apple pie.png",
+    stars: "★★★★☆",
+    price: 687
+  },
+  {
+    id: "usa-2",
+    name: "Fried Chicken",
+    image: "american foods/fried chicken.png",
+    stars: "★★★★★",
+    price: 1189
+  },
+  {
+    id: "usa-3",
+    name: "Hamburger",
+    image: "american foods/hamburger.png",
+    stars: "★★★★★",
+    price: 1048
+  },
+  {
+    id: "usa-4",
+    name: "Hot Dog",
+    image: "american foods/hot dog.png",
+    stars: "★★★★☆",
+    price: 719
+  },
+  {
+    id: "usa-5",
+    name: "Mac and Cheese",
+    image: "american foods/mac and cheese.png",
+    stars: "★★★★☆",
+    price: 834
+  }
+];
+
+function foods(countryFood){
+  let html='';
+
+  countryFood.forEach((item)=>{
+  html+=`<div class="grid-container">
+  <div>
+    <img src="${item.image}">
+
+            </div>
+            <div class="rating">
+            <span class="rating-name">
+            <span>
+            Rating:
+            </span>
+                <span class="stars-rating">
+             ${item.stars}
+
+                </span>
+                </span>
+                <span class="price">
+                Price:₹${item.price}
+
+                </span>
+                
+            </div>
+            </div>`;
+
+  });
+  return html;
+
+};
+let indHtml = foods(indianFoods);
+let swizHtml=foods(swizFood);
+let americaHtml=foods(americanFoods);
+let southKoreanHtml=foods(southKoreaFoods);
+let japanHtml=foods(japanFoods);
+
+document.querySelector('.js-indian-foods').innerHTML=indHtml;
+document.querySelector('.js-swiz-foods').innerHTML=swizHtml;
+document.querySelector('.js-american-foods').innerHTML=americaHtml;
+document.querySelector('.js-southKorean-foods').innerHTML=southKoreanHtml;
+document.querySelector('.js-japan-foods').innerHTML=japanHtml;
+ function foodMotion(){
+const rows = document.querySelectorAll(".foods-grid");
+
+
+
+rows.forEach((row) => {
+    row.innerHTML += row.innerHTML;
+    
+    
+});
+ };
+ foodMotion();
+ 
+
+ document.querySelector("#book").addEventListener("click",()=>{
+  console.log("hello");
+  document.querySelector(".model").classList.add("model-popup");
+  document.querySelector(".model-content").classList.add("reservation-popup")
+
+    document.querySelector(".reservation").classList.add("reservation-popup")
+
+ })
+ document.querySelector("#reserve").addEventListener("click",()=>{
+  console.log("hello");
+  document.querySelector(".model").classList.remove("model-popup");
+  document.querySelector(".model-content").classList.remove("reservation-popup")
+
+    document.querySelector(".reservation").classList.remove("reservation-popup")
+
+ })
+ 
+
+ 
